6869d83f5aee8d82c73befaa2b093073252b6c43
Daniel Trotter git config --globalgit config --global user.email flare84@gmail.comgit config --global user.email flare84@gmail.com <flare84@gmail.comgit config --global user.email flare84@gmail.comgit config --global user.name Daniel>
2018-08-30 12:22:25 -0600

    update controller methods and views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,4 +10,20 @@ class PagesController < ApplicationController
   def new
     @page = Page.new
   end
+
+  def Create
+    @page = Page.new(page_params)
+
+    if @page.save
+      #redirect somewhere
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def page_params
+      params.require(:page).permit(:title, :author, :body)
+    end
 end
